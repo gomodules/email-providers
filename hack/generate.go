660608130f08e916e61b9dddde963e67d0e73e0f
Tamal Saha <tamal@appscode.com>
2020-08-11 17:58:29 -0700

    Add email provider list
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ hack/generate.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+// https://github.com/wesbos/burner-email-providers
+func main() {
+	resp, err := http.Get("https://github.com/wesbos/burner-email-providers/raw/master/emails.txt")
+	if err != nil {
+		panic(err)
+	}
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	resp.Body.Close()
+
+	var out bytes.Buffer
+	out.WriteString(`package emailproviders
+
+// https://github.com/wesbos/burner-email-providers
+var disposableEmailServices = map[string]struct{}{
+`)
+
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		line := strings.TrimSpace(line)
+		if line != "" {
+			out.WriteString(fmt.Sprintf(`"%s": empty,`, line))
+			out.WriteRune('\n')
+		}
+	}
+
+	out.WriteString(`
+}`)
+
+	err = ioutil.WriteFile("./disposable_email_services.go", out.Bytes(), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
